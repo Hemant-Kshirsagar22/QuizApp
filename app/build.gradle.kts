bf95357c0ac9d8a0d2103def31d9e6de00172e27
Hemant-Kshirsagar22 <hemantkshirsagar23@gmail.com>
2025-01-12 23:02:10 +0530

    added registration form insertion operation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.quizapp"
+    namespace = "com.project.quizapp"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.quizapp"
+        applicationId = "com.project.quizapp"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
