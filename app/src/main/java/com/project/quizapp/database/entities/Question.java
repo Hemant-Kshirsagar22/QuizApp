0fd9c8f9942b866dcda128f2da2aec762596dad9
Hemant-Kshirsgar22 <hemantkshirsagar23@gmail.com>
2025-01-30 19:53:28 +0530

    Question Getting Function implemented

---
+++ app/src/main/java/com/project/quizapp/database/entities/Question.java
@@ -0,0 +1,51 @@
+package com.project.quizapp.database.entities;
+
+import java.util.ArrayList;
+
+public class Question {
+    private String question;
+    private ArrayList<String> options;
+    private String answer;
+
+    public Question() {}
+
+    public Question(String question, ArrayList<String> options, String answer) {
+        this.question = question;
+        this.options = options;
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public ArrayList<String> getOptions() {
+        return options;
+    }
+
+    public void setOptions(ArrayList<String> options) {
+        this.options = options;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "question='" + question + '\'' +
+                ", options=" + options +
+                ", answer='" + answer + '\'' +
+                '}';
+    }
+}
