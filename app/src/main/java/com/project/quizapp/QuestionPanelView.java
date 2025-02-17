deff36b444922417bd2ee9866ff253361bd036bd
Hemant-Kshirsagar22 <hemantkshirsagar23@gmail.com>
2025-02-17 23:35:40 +0530

    Questions Backend Updates

--- app/src/main/java/com/project/quizapp/QuestionPanelView.java
+++ app/src/main/java/com/project/quizapp/QuestionPanelView.java
@@ -1,24 +1,18 @@
 package com.project.quizapp;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.LinearLayout;
+import android.util.Log;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.project.quizapp.database.FirebaseDBHelper;
 import com.project.quizapp.database.entities.Question;
-import com.project.quizapp.databinding.ActivityMainBinding;
 import com.project.quizapp.databinding.ActivityQuestionPanelViewBinding;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 
@@ -33,6 +27,7 @@ public class QuestionPanelView extends AppCompatActivity {
     private final String C = "C";
     private final String D = "D";
 
+    private HashMap<Integer,String> answerList = null;
 
     private int currentQuetionPosition = 0;
 
@@ -45,52 +40,13 @@ public class QuestionPanelView extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         radioGroup = (RadioGroup) binding.radioGroup;
+        answerList = new HashMap<Integer,String>();
 
         FirebaseDBHelper.getQuestionByCategory("Logical-Reasoning/Analogies", new FirebaseDBHelper.QuestionQueryCallback() {
             @Override
             public void onSuccess(List<Question> question) {
                 questions = question;
-                Toast.makeText(getApplicationContext(), "Fetch Success", Toast.LENGTH_SHORT).show();
-                if(questions != null)
-                {
-                    binding.tvQuestion.setText(questions.get(currentQuetionPosition).getQuestion());
-                    binding.tvQuestionNumber.append(currentQuetionPosition + 1 + "");
-
-                    List options = questions.get(currentQuetionPosition).getOptions();
-
-                    binding.a.append(options.get(0).toString());
-                    binding.b.append(options.get(1).toString());
-                    binding.c.append(options.get(2).toString());
-                    binding.d.append(options.get(3).toString());
-
-
-                    radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-                        @Override
-                        public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                            if(binding.a.isChecked())
-                            {
-//                                Toast.makeText(getApplicationContext(), "A", Toast.LENGTH_SHORT).show();
-                                setupOptionSelection(binding.a, A);
-                            }
-                            else if(binding.b.isChecked())
-                            {
-//                                Toast.makeText(getApplicationContext(), "B", Toast.LENGTH_SHORT).show();
-                                setupOptionSelection(binding.b, B);
-                            }
-                            else if(binding.c.isChecked())
-                            {
-//                                Toast.makeText(getApplicationContext(), "C", Toast.LENGTH_SHORT).show();
-                                setupOptionSelection(binding.c, C);
-                            }
-                            else if(binding.d.isChecked())
-                            {
-//                                Toast.makeText(getApplicationContext(), "D", Toast.LENGTH_SHORT).show();
-                                setupOptionSelection(binding.d, D);
-                            }
-
-                        }
-                    });
-                }
+                changeQuestion();
             }
 
             @Override
@@ -100,6 +56,57 @@ public class QuestionPanelView extends AppCompatActivity {
         });
 
 
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if(binding.a.isChecked())
+                {
+                    answerList.put(currentQuetionPosition, A);
+                    setupOptionSelection(binding.a, A);
+                }
+                else if(binding.b.isChecked())
+                {
+                    answerList.put(currentQuetionPosition, B);
+                    setupOptionSelection(binding.b, B);
+                }
+                else if(binding.c.isChecked())
+                {
+                    answerList.put(currentQuetionPosition, C);
+                    setupOptionSelection(binding.c, C);
+                }
+                else if(binding.d.isChecked())
+                {
+                    answerList.put(currentQuetionPosition, D);
+                    setupOptionSelection(binding.d, D);
+                }
+
+            }
+        });
+
+        binding.clearResponse.setOnClickListener(view -> {
+            clearOptions();
+            answerList.put(currentQuetionPosition, null);
+            Log.d("ANS",answerList.toString());
+        });
+
+        binding.next.setOnClickListener(view -> {
+            currentQuetionPosition++;
+            if(currentQuetionPosition == questions.size())
+            {
+                currentQuetionPosition = 0;
+            }
+            changeQuestion();
+        });
+
+        binding.Previous.setOnClickListener(view -> {
+            currentQuetionPosition--;
+            if(currentQuetionPosition < 0)
+            {
+                currentQuetionPosition = questions.size() - 1;
+            }
+            changeQuestion();
+        });
+
         // Handle option selection
 //        setupOptionSelection(binding.optionA, "430");
 //        setupOptionSelection(binding.optionB, "420");
@@ -107,7 +114,45 @@ public class QuestionPanelView extends AppCompatActivity {
 //        setupOptionSelection(binding.optionD, "435"); // Correct Answer
     }
 
+    private void changeQuestion()
+    {
+        if(questions != null)
+        {
+            binding.tvQuestion.setText(questions.get(currentQuetionPosition).getQuestion());
+            binding.tvQuestionNumber.setText("Question : " + (currentQuetionPosition + 1));
+
+            List<String> options = questions.get(currentQuetionPosition).getOptions();
+
+            binding.a.setText(A + " " + options.get(0));
+            binding.b.setText(B + " " + options.get(1));
+            binding.c.setText(C + " " + options.get(2));
+            binding.d.setText(D + " " + options.get(3));
+
+            clearOptions();
+
+            String ans = null;
+            if((ans = answerList.get(currentQuetionPosition)) != null)
+            {
+                switch (ans)
+                {
+                    case A:
+                        binding.a.setBackgroundResource(R.drawable.option_background_selected);
+                        break;
+                    case B:
+                        binding.b.setBackgroundResource(R.drawable.option_background_selected);
+                        break;
+                    case C:
+                        binding.c.setBackgroundResource(R.drawable.option_background_selected);
+                        break;
+                    case D:
+                        binding.d.setBackgroundResource(R.drawable.option_background_selected);
+                        break;
+                }
+            }
+        }
+    }
     private void setupOptionSelection(RadioButton option, String answer) {
+        Log.d("ANS",answerList.toString());
         if(Objects.equals(option, binding.a))
         {
             option.setBackgroundResource(R.drawable.option_background_selected);
@@ -145,7 +190,15 @@ public class QuestionPanelView extends AppCompatActivity {
         if (answer.equals(questions.get(currentQuetionPosition).getAnswer())) {
             Toast.makeText(this, "Correct Answer", Toast.LENGTH_SHORT).show();
         } else {
-            Toast.makeText(this, "Wrong Answer", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void clearOptions()
+    {
+        binding.a.setBackgroundResource(R.drawable.option_background);
+        binding.b.setBackgroundResource(R.drawable.option_background);
+        binding.c.setBackgroundResource(R.drawable.option_background);
+        binding.d.setBackgroundResource(R.drawable.option_background);
+
     }
 }
