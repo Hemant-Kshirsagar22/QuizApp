17a5a2f76cfb6438912fcd0c164b4ed5dcc1702d
Sushant-Chavan <130270903+Sushant3925@users.noreply.github.com>
2025-02-17 19:07:45 +0530

    Updated activity_question_panel_view.xml for night and light mode

--- app/src/main/java/com/project/quizapp/Dashboard.java
+++ app/src/main/java/com/project/quizapp/Dashboard.java
@@ -55,6 +55,17 @@ public class Dashboard extends GlobalDrawerLayoutAndBottomNavigation {
 //                }
 //
             }
+
+
+        });
+
+        binding.LogicalReasoning.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Dashboard.this, "LogicalReasoning", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Dashboard.this, QuestionPanelView.class);
+                startActivity(intent);
+            }
         });
 
     }
