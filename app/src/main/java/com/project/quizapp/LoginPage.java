c4733f9b8267e93696e7af91ebc5f25e54690eb7
Sushant-Chavan <130270903+Sushant3925@users.noreply.github.com>
2025-04-07 16:33:14 +0530

    Done with double backpress performance handleled the exit problem

--- app/src/main/java/com/project/quizapp/LoginPage.java
+++ app/src/main/java/com/project/quizapp/LoginPage.java
@@ -4,6 +4,7 @@ import static com.project.quizapp.database.Status.MSG_LOGIN_SUCCESS;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -39,7 +40,8 @@ public class LoginPage extends AppCompatActivity{
 
     private GoogleSignInClient googleLoginClient;
     private static final int RC_SIGN_IN = 9001;
-
+    private boolean doubleBackToExitPressedOnce = false;
+    private Toast exitToast;
     int counter=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -142,6 +144,27 @@ public class LoginPage extends AppCompatActivity{
                 Log.w("GOOGLE_LOGIN", "Google sign-in failed : " + e.getMessage(), e);
                 Toast.makeText(this, "Google sign-in failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
             }
+        }
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+        if (getFragmentManager().getBackStackEntryCount() > 0) {
+            getFragmentManager().popBackStack();
+        } else {
+            if (doubleBackToExitPressedOnce) {
+                if (exitToast != null) exitToast.cancel();
+                super.onBackPressed(); // This will exit the app
+                return;
+            }
+
+            doubleBackToExitPressedOnce = true;
+            exitToast = Toast.makeText(this, "Press again to exit", Toast.LENGTH_SHORT);
+            exitToast.show();
+
+            new Handler().postDelayed(() -> doubleBackToExitPressedOnce = false, 2000);
         }
     }
 
